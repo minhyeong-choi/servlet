f77e89c4cfda577b772a76ffcd62c970b6c2afbb
minhyeong-choi <minhyeong5112@gmail.com>
2026-09-09 08:36:17 +0900

    feat: 서블릿 MVC V4 구조 구현
    - ControllerV4 인터페이스 정의
    - MyView 수정
    - FrontControllerServletV4 구현
    - V4 기반 회원 관리 컨트롤러 추가 (Form, List, Save)

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -22,12 +22,11 @@ public class MyView {
 
     public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         modelToRequestAttribute(model, request);
-
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
 
-    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
         model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
