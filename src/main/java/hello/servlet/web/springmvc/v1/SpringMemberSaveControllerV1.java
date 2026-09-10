c1d130cf33516f890d1148e2286c5cb797e877e0
minhyeong-choi <minhyeong5112@gmail.com>
2026-09-10 10:21:25 +0900

    feat: 스프링 MVC 추가

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,26 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(Map<String, String> paramMap) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+        Member member = new Member(username, age);
+
+        memberRepository.save(member);
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+
+}
